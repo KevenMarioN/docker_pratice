b72de2ca182d80d4d68a97c128f8aad732ee8637
KevenMarioN <kevenmario.n.r@gmail.com>
2022-04-07 11:35:30 -0300

    feat: waiting db really okay, for running app

--- node/index.js
+++ node/index.js
@@ -1,7 +1,20 @@
 const express = require('express');
+const mysql = require('mysql');
+
 const app = express();
 const PORT = 5000;
+const config = {
+  host : 'db',
+  user : 'root',
+  password : 'root',
+  database : 'nodedb'
+};
 
+const connection = mysql.createConnection(config);
+
+const sql = `INSERT INTO people(name) values('Keven')`
+connection.query(sql);
+connection.end();
 app.get('/',(req,res) => {
   res.send(`<h1>Hello World</h1>`).status(200);
 });
