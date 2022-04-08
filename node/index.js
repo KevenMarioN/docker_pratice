3ecb0d2b9d5c92a9ad83838dff845b53dad32e7f
KevenMarioN <kevenmario.n.r@gmail.com>
2022-04-08 11:13:14 -0300

    correção

--- node/index.js
+++ node/index.js
@@ -12,11 +12,18 @@ const config = {
 
 const connection = mysql.createConnection(config);
 
-const sql = `INSERT INTO people(name) values('Keven')`
+const sql = `INSERT INTO people(name) values('Keven')`;
+const sqlSelect = `SELECT * FROM people;`
 connection.query(sql);
-connection.end();
-app.get('/',(req,res) => {
-  res.send(`<h1>Hello World</h1>`).status(200);
+app.get('/', async (req,res) => {
+let peoples;
+ await connection.query(sqlSelect,function (error,results,fields){
+    if (error) throw error;
+    const data = results.map((result) => result.name);
+  });
+  res.send(`
+  <h1>Hello World</h1>
+  `).status(200);
 });
 
 app.listen(PORT,() => console.log(`🔥 Server running in port ${PORT}`));
